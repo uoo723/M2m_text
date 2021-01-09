af1adf761219c10ef37b3c6179dac958b58889f8
Sangwoo Han <realappsdev@gmail.com>
2021-01-09 14:09:00 +0900

    Changed config files directory

--- scripts/run_drugreview_baseline.sh
+++ scripts/run_drugreview_baseline.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 args=(
-    --model-cnf config/AttentionRNN.yaml
-    --data-cnf config/DrugReview.yaml
+    --model-cnf config/models/AttentionRNN.yaml
+    --data-cnf config/datasets/DrugReview.yaml
     --epoch 200
     --lr 0.001
     --train-batch-size 800
