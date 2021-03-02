20b35000a415a07e619611faa8eb4a428a7c868d
Sangwoo Han <realappsdev@gmail.com>
2021-03-02 12:09:18 +0900

    add double mixup script

---
+++ scripts/mixup/run_eurlex_300_attention_rnn_double_mixup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+args=(
+    --model-cnf config/models/AttentionRNN_EURLex-300.yaml
+    --data-cnf config/datasets/EURLex-300.yaml
+    --mixup-enabled
+    --double-mixup-enabled
+    --warm 50
+    --epoch 150
+    --lr 1e-3
+    --eta-min 1e-5
+    --train-batch-size 100
+    --test-batch-size 500
+    --ckpt-name Double_Mixup
+    --no-over
+    --no-over-gen
+    --early-criterion 'p5'
+    --seed $1
+    --swa-warmup -1
+    --eval-step 50
+    --early 50
+)
+
+python main.py "${args[@]}"
