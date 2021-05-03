fa2606bdcd321427083c989472987695d22fa4db
Sangwoo Han <realappsdev@gmail.com>
2021-05-03 15:30:53 +0900

    Allow zero in degree

--- m2m_text/networks.py
+++ m2m_text/networks.py
@@ -911,7 +911,7 @@ class LabelGCNAttentionRNNv4(AttentionRNN):
             assert gcn_init_adj is not None, "gcn_init_adj must be set when use GAT."
             gcl_hidden_size = [label_emb_size] + gcn_hidden_size
             g = dgl.from_scipy(sp.csr_matrix(gcn_init_adj.numpy()))
-            self.g = dgl.add_self_loop(g)
+            # self.g = dgl.add_self_loop(g)
             self.gcl = nn.ModuleList(
                 GATConv(
                     in_s,
@@ -920,6 +920,7 @@ class LabelGCNAttentionRNNv4(AttentionRNN):
                     feat_drop=gcn_dropout,
                     residual=True,
                     activation=F.relu,
+                    allow_zero_in_degree=True,
                 )
                 for in_s, out_s in zip(gcl_hidden_size[:-1], gcl_hidden_size[1:])
             )
